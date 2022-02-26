1801cfe1d879e91029a13ccd0b91c5c7c90800e3
Spandan <royspandan1@gmail.com>
2022-02-26 10:02:34 +0530

    Added UI sugar

--- sudoku.c
+++ sudoku.c
@@ -85,8 +85,16 @@ void print_sudoku(int sudoku[9][9]){
 	for(int i = 0; i < 9; i++){
 		for(int j = 0; j < 9; j++){
 			printf("%d ", sudoku[i][j]);
+			if(j == 2 || j == 5){
+				printf("| ");
+			}
 		}
-		printf("\n");
+		if (i == 2 || i == 5){
+			printf("\n---------------------\n");
+		}
+		else{
+			printf("\n");
+		}
 	}
 }
 
